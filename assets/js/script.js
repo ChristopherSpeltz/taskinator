de30a68522292819c92a706a5601214b6b82fa1b
Christopher Speltz <chrisspeltz@msn.com>
2021-10-27 18:24:27 -0500

    save the edited task

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,35 @@ var taskFormHandler = function(event) {
         return false;        
     }
 
-    // package up data as an object
+    var isEdit = formEl.hasAttribute("data-task-id");
+// has data attribute, so get task id and call function to complete edit process
+if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+} 
+  // no data attribute, so create object as normal and pass to createTaskEl function
+else {
     var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
+    name: taskNameInput,
+    type: taskTypeInput
     };
 
-    formEl.reset();
-
-    // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
+}
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -32,7 +51,7 @@ var createTaskEl = function (taskDataObj) {
     listItemEl.className = "task-item";
 
      // add task id as a custom attribute
-  listItemEl.setAttribute("data-task-id", taskIdCounter);
+listItemEl.setAttribute("data-task-id", taskIdCounter);
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
