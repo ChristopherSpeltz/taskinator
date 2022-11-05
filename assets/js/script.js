367a444fda679e863514d1656ddd17e726988251
Christopher Speltz <chrisspeltz@msn.com>
2022-11-04 22:32:00 -0500

    4.2.8 address usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ var taskFormHandler = function (event) {
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  //   check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
@@ -15,7 +24,6 @@ var taskFormHandler = function (event) {
 
   //   sent it as an argument to createTaskEl
   createTaskEl(taskDataObj);
-  console.dir(listItemEl);
 };
 
 var createTaskEl = function (taskDataObj) {
